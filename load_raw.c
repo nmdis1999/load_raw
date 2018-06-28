d8e75a7e466f36af095b06a35617c5a469cc1eae
Iti Shree <itishree1999@gmail.com>
2018-06-28 20:28:24 +0530

    Added comments; removed unused variables

--- load_raw.c
+++ load_raw.c
@@ -16,53 +16,48 @@
 
 static char *cmd_name = NULL;
 
+
 static uint32_t map_base = 0x18000000;
 static uint32_t map_size = 0x08000000;
 
 static uint32_t map_addr = 0x00000000;
 
+
 static char *dev_mem = "/dev/mem";
 
-static bool opt_raw = false;
-static bool opt_pixel = false;
-static bool opt_overlay = false;
-
-static uint32_t num_tpat = 0;
-
-/*static inline
-uint64_t calc_tpat(unsigned num, unsigned col, unsigned row) {
-    switch(num) {
-         }
-    }
-}*/
-
+static bool opt_raw = false;  /* To check if the image is raw, which should be default in this tool */
 
 static
 void write_line(uint8_t *bp, uint64_t *dp, unsigned row) {
-    uint64_t val = 0;
+    uint64_t val = 0; /*  */
     uint64_t mask = 0;
     unsigned inc;
 
     inc = 3;  /* Step-size for RGB */
 
-    mask = ~0xFFFF;
+    mask = ~0xFFFF; /*Using mask of ~0xFFFF because 0xFFFF is used to mask the overlay */
 
-    for (unsigned col = 0; col < 2048; col++) {
-        /* copying data from the source */
+/* Iterating over full HD */
+    for (
+            unsigned col = 0;
+            col < 2048; col++) {
+/* copying data from the source */
 
         val = bp[0];
-        val = (val << 8) | bp[1];
+        val = (val << 8) | bp[1];   /* Loads one byte after another into val */
         val = (val << 8) | bp[2];
         val = (val << 8) | bp[3];
         val = (val << 8) | bp[4];
         val = (val << 8) | bp[5];
-        val = (val << 16);
+        val = (val << 16);         /* After 6 bytes, shift the input by 16 skipping over the overlay */
 
-        *dp = (*dp & ~mask) | (val & mask);
-        dp++;
-        bp += inc;
+        *
+                dp = (*dp & ~mask) | (val & mask); /* Data is masked out and then modified into memory */
+/* First we read data from frame buffer and then mask it with complementary mask */
+        dp++;  /* Incrementing data pointer */
+        bp +=
+                inc; /* Incrementing buffer pointer by steo-size i.e 3 */
     }
-    printf("%u\n", dp);
 }
 
 int main(int argc, char **argv) {
@@ -70,7 +65,7 @@ int main(int argc, char **argv) {
     extern char *optarg;
     int c, err_flag = 0;
 
-#define    OPTIONS "hr"
+#define    OPTIONS "hr:B:S"
 
     cmd_name = argv[0];
     while ((c = getopt(argc, argv, OPTIONS)) != EOF) {
@@ -79,19 +74,26 @@ int main(int argc, char **argv) {
                 fprintf(stderr,
                         "options are:\n"
                         "-h        print this help message\n"
-                        "-r        load raw data\n", cmd_name);
+                        "-r        load raw data\n"
+                        "-B <val>  memory mapping base\n"
+                        "-S <val>  memory mapping size\n", cmd_name);
                 exit(0);
                 break;
 
             case 'r':
                 opt_raw = true;
                 break;
+            case 'B' :
+                map_base = strtoll(optarg, NULL, 16); /* produces a number of type long long int */
+            case 'S' :
+                map_size = strtoll(optarg, NULL, 16);
             default:
                 err_flag++;
                 break;
         }
     }
 
+    /* If no option is matched print this message */
     if (err_flag) {
         fprintf(stderr,
                 "Usage: %s -[" OPTIONS "] [file]\n"
@@ -100,7 +102,7 @@ int main(int argc, char **argv) {
         exit(2);
     }
 
-
+    /* Opening dev/mem with read/write permission */
     int fd = open(dev_mem, O_RDWR | O_SYNC);
     if (fd == -1) {
         fprintf(stderr,
@@ -109,10 +111,10 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    /* Mapping of frame buffer starts from here */
     if (map_addr == 0)
         map_addr = map_base;
 
+    /* Mapping base for memory allocation */
     void *base = mmap((void *) map_addr, map_size,
                       PROT_READ | PROT_WRITE, MAP_SHARED,
                       fd, map_base);
@@ -124,22 +126,17 @@ int main(int argc, char **argv) {
         exit(2);
     }
 
+    /* Printing message after sucessfully mapping base */
     fprintf(stderr,
             "mapped 0x%08lX+0x%08lX to 0x%08lX.\n",
             (long unsigned) map_base, (long unsigned) map_size,
-            (long unsigned) base);   /* Mapping ends */
+            (long unsigned) base);
 
-    // Checking if filename is supplied if yes, open as stdin
-
+    /* Checking if filename was supplied, if so, open as stdin */
     if (argc > optind) {
         close(0);
         open(argv[optind], O_RDONLY, 0);
     }
-
-    /* line 464-466
-     * fprintf(stderr,
-        "read buffer = 0x%08lX\n",
-         (unsigned long)buf_base[0]);*/
 
     // Iteration over every row count of 1536 for raw image format
 
@@ -151,31 +148,34 @@ int main(int argc, char **argv) {
 
         buf_size = 2048 * 3;
 
-        /* Checking if we want to load the data from the file */
-        if ((num_tpat == 0) && !opt_pixel) {
-            size_t total = 0;
-            while (total < buf_size) {
-                size_t len = read(0, bp, buf_size - total);
+        /* loading the data from the file */
 
-                if (len == 0)
-                    exit(1);
-                if (len < 0)
-                    exit(2);
+        size_t total = 0;
+        while (total < buf_size) {
+            size_t len = read(0, bp, buf_size - total);
 
-                total += len;
-                bp += len;
-            }
+            if (len == 0)
+                exit(1);
+            if (len < 0)
+                exit(2);
+
+            total += len;
+            bp += len;
         }
 
         bp = buf;
 
         // Only calling write_line() when we have to write active raw buffer
 
-        uint32_t dp_base = map_addr + (buf_base[0] - map_base);
+        uint32_t dp_base = map_addr;
         uint64_t *dp = (uint64_t *) (dp_base + row * 16384);  /* dp stands for data pointer */
-
-        write_line(bp, dp, row);
+        /* frame buffer has 16384 bytes per row b/c 16384/2048=8 */
+        /* We will put 4 pixel in a pack and address that as 64 bit int */
+        write_line(bp, dp, row); /* Using write_line() to write into the memory */
     }
 
+    printf("%u\n", map_base);
+
     exit((err_flag) ? 1 : 0);
+
 }
